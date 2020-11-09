e12a9c26e464d80dc5d97a429d63ba17b6391d60
iamjayy <jason_doan94@yahoo.com>
2020-11-09 17:49:08 -0500

    redux

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
 import postRoutes from "./routes/post.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 // https://www.mongodb.com/cloud/atlas
 
