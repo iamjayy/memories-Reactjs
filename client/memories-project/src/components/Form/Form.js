5264a894d9992a78a046df76574446e4c70f5899
iamjayy <jason_doan94@yahoo.com>
2020-11-10 00:11:42 -0500

    MERN

--- client/memories-project/src/components/Form/Form.js
+++ client/memories-project/src/components/Form/Form.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
 
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -13,7 +15,13 @@ const Form = () => {
   });
 
   const classes = useStyles();
-  const handleSubmit = () => {};
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createPost(postData));
+  };
 
   const clear = () => {};
 
