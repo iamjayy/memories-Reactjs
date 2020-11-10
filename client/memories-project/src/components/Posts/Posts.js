418b38e8e6b63fdeef61c5e601bbd00701463b25
iamjayy <jason_doan94@yahoo.com>
2020-11-10 01:10:43 -0500

    post

--- client/memories-project/src/components/Posts/Posts.js
+++ client/memories-project/src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Post from "./Post/Post";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 import useStyles from "./styles";
 
@@ -10,12 +11,21 @@ const Posts = () => {
 
   console.log(posts);
 
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
