3de085284d8e4b5f6d60a1919ad4652bb57a27ba
iamjayy <jason_doan94@yahoo.com>
2020-11-10 18:28:01 -0500

    delete

--- client/memories-project/src/actions/posts.js
+++ client/memories-project/src/actions/posts.js
@@ -1,4 +1,4 @@
-import * as api from "../api";
+import * as api from "../api/index";
 
 // action creators
 export const getPosts = () => async (dispatch) => {
@@ -27,5 +27,15 @@ export const updatePost = (id, post) => async (dispatch) => {
     dispatch({ type: "UPDATE", payload: data });
   } catch (error) {
     console.log(error.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: "DELETE", payload: id });
+  } catch (error) {
+    console.log(error);
   }
 };
--- client/memories-project/src/reducers/posts.js
+++ client/memories-project/src/reducers/posts.js
@@ -2,6 +2,8 @@ const { Switch } = require("@material-ui/core");
 
 export default (posts = [], action) => {
   switch (action.type) {
+    case 'DELETE':
+      return posts.filter((post) => post._id !=== action.payload)
     case "UPDATE":
       return posts.map((post) =>
         posts._id === action.payload._id ? action.payload : post
