7286195b16745575d787ea2fedced568fda28862
iamjayy <jason_doan94@yahoo.com>
2020-11-10 18:42:25 -0500

    dotenv

--- server/index.js
+++ server/index.js
@@ -2,29 +2,29 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
 import postRoutes from "./routes/post.js";
 
 const app = express();
+dotenv.config();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postRoutes);
-
-// https://www.mongodb.com/cloud/atlas
-
-const CONNECTION_URL =
-  "mongodb+srv://whos_jayy:1drose23@cluster0.dfuav.mongodb.net/memories?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
+    app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
   )
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(error.message));
 
 mongoose.set("useFindAndModify", false);
